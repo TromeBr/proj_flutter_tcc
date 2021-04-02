dfd0e0aa370eb5185ea31c9c5e23bbfacaf4616e
TromeBr <gabriel.flimasilva@gmail.com>
2021-04-02 17:50:00 -0300

    Criação da Tela de login de forma básica (Refatorar futuramente)

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,236 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(TCCApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class TCCApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'TCC Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        body: TelaLogin(),
       ),
-      home: MyHomePage(title: 'Aplicativo TCC Demo'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
+class TelaLogin extends StatelessWidget {
+  final TextEditingController _LoginUser = TextEditingController();
+  final TextEditingController _LoginPassword = TextEditingController();
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(32.0),
+        ),
+        Container(
+          child: Image.asset(
+            'assets/images/PUC_SP.png',
+            height: 120,
+            width: 120,
+            fit: BoxFit.contain,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+            controller: _LoginUser,
+            style: TextStyle(fontSize: 24.0),
+            decoration: InputDecoration(
+              labelText: 'Nome de usuário',
+              hintText: 'Usuário',
+              icon: Icon(
+                Icons.account_circle_sharp,
+                color: Colors.blueAccent,
+              ),
+            ),
+            keyboardType: TextInputType.text,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+            controller: _LoginPassword,
+            style: TextStyle(fontSize: 24.0),
+            decoration: InputDecoration(
+              labelText: 'Senha',
+              hintText: '******',
+            ),
+            keyboardType: TextInputType.text,
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+        ),
+        ElevatedButton(
+          child: Text('Login'),
+          onPressed: () {
+            final String _user = _LoginUser.text;
+            final String _password = _LoginPassword.text;
+            if (_user != null && _password != null) {
+              final loginFinal = LoginUsuario(_user, _password);
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text('$loginFinal'),
+                ),
+              );
+              debugPrint('$loginFinal');
+            } else {}
+          },
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+        ),
+        Text('Ou'),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+        ),
+        ElevatedButton(child: Text('Registre-se'), onPressed: null),
+      ],
+    ));
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class LoginUsuario {
+  final String loginUser;
+  final String loginPassword;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  LoginUsuario(this.loginUser, this.loginPassword);
+
+  @override
+  String toString() {
+    return 'LoginUsuario{User: $loginUser, Password: $loginPassword}';
   }
+}
+
+class FormularioTransferencia extends StatelessWidget {
+  final TextEditingController _controladorCampoValor = TextEditingController();
+  final TextEditingController _controladorCampoNumConta =
+      TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Criando Transferência'),
+        backgroundColor: Colors.deepPurple,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _controladorCampoNumConta,
+              style: TextStyle(fontSize: 24.0),
+              decoration: InputDecoration(
+                labelText: 'Núm. Conta',
+                hintText: '0000',
+                icon: Icon(
+                  Icons.account_circle_sharp,
+                  color: Colors.deepPurple,
+                ),
+              ),
+              keyboardType: TextInputType.number,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+                controller: _controladorCampoValor,
+                style: TextStyle(
+                  fontSize: 24.0,
+                ),
+                decoration: InputDecoration(
+                  labelText: 'Valor',
+                  hintText: '0.00',
+                  icon: Icon(
+                    Icons.monetization_on_sharp,
+                    color: Colors.deepPurple,
+                  ),
+                ),
+                keyboardType: TextInputType.number),
+          ),
+          ElevatedButton(
+            child: Text('Confirmar'),
+            onPressed: () {
+              final int numConta = int.tryParse(_controladorCampoNumConta.text);
+              final double valor = double.tryParse(_controladorCampoValor.text);
+              if (numConta != null && valor != null) {
+                final transferenciaCriada = Transferencia(valor, numConta);
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('$transferenciaCriada'),
+                  ),
+                );
+                debugPrint('$transferenciaCriada');
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Transferências'),
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: Column(
+        children: [
+          ItemTransferencia(Transferencia(100.0, 1000)),
+          ItemTransferencia(Transferencia(200.0, 2000)),
+          ItemTransferencia(Transferencia(300.0, 3000)),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        backgroundColor: Colors.deepPurple,
+        onPressed: () {},
+      ),
     );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transf;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transf.valor.toString()),
+        subtitle: Text(_transf.numConta.toString()),
+      ),
+    );
+  }
+
+  ItemTransferencia(this._transf);
+}
+
+class Transferencia {
+  final double valor;
+  final int numConta;
+
+  Transferencia(this.valor, this.numConta);
+
+  @override
+  String toString() {
+    return 'Transferencia{valor: $valor, numConta: $numConta}';
   }
 }
