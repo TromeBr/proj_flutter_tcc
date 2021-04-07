fa50b2670965ad528d2d806957b43b76d080dc37
TromeBr <gabriel.flimasilva@gmail.com>
2021-04-06 22:45:29 -0300

    Tela de Registro de um usuário novo

---
+++ lib/screens/login/register.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+
+class UserRegisterScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return UserRegisterWidgetState();
+  }
+}
+
+
+class UserRegisterWidgetState extends State<UserRegisterScreen>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+}
